39764be9c9c62abfa48aedfc78a163958d794cba
MoniMurray <>
2023-05-06 13:19:36 +0000

    Add Products code Part4, add items by size and id to shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -14,15 +14,29 @@ def bag_contents(request):
     # iterate through all the items in the shopping bag, and along the way tally up the
     # total cost and product count and add the products and their data to the bag items list.
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        # only run this logic if the item has no sizes ie. item data will be an int
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+            # otherwise, if the item data is a dictionary we know there are sizes
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += item_data
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
