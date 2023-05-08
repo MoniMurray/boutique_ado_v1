42a547bc0e758e319c4a847096bc24a8dcedb07a
Monica Murray <Monica.rickerby@gmail.com>
2023-05-08 20:33:25 +0000

    Add OrderAdmin and inline OrderLineItemAdmin, and signals.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """ display the OrderLineItem model fields within the same page displaying the Order model fields"""
+
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+class OrderAdmin(admin.ModelAdmin):
+    """ Set up the Admin view of the fields in Order model """
+
+    inlines = (OrderLineItemAdminInline,)
+
+    # these will all be calculated by our model methods so we don't want anyone to change them
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+
+    # not 'strictly' necessary to do the following, but if you want your fields in a specific order you should do this
+    fields = (
+        'order_number',
+        'date',
+        'full_name',
+        'email',
+        'phone_number',
+        'country',
+        'post_code',
+        'town_or_city',
+        'street_address1',
+        'street_address2',
+        'county',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'order_total',
+        'delivery_cost',
+        'grand_total',
+    )
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
