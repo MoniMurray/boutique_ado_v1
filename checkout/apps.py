71d0b8af1e910476e6dc6e35dafc609d91fcef46
Monica Murray <Monica.rickerby@gmail.com>
2023-05-08 20:37:08 +0000

    Override the ready method by importing signals module into apps.py

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    # to override the ready method by importin our signals module,
+    #now every time a lineitem is saved or deleted our custom Update_total() 
+    #model method will be called, updating the order totals automatically
+    def ready(self):
+        import checkout.signals
