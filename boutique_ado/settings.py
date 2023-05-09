3eb9c1c060b57adff3030815be4e1b7e2d9cb586
Monica Murray <Monica.rickerby@gmail.com>
2023-05-09 21:49:42 +0000

    Add Checkout views, urls, install Crispy Forms, checkout template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +79,18 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import Orderform 
 
 # Create your views here.
+def checkout(request):
+    """ Get the shopping bag from the session; 
+    if there's nothing in the bag return a simple error message
+    and redirect back to the products page; 
+    create an instance of Orderform, which will be empty for now, 
+    and then create the template, the context containing the Orderform,
+    and finally render it all out. """
+
+    bag = request.session.get('bag', {})
+    # if nothing in the bag
+    if not bag:
+        messages.error(request, "There is nothing in your bag at the moment")
+        return redirect(reverse('products'))
+    
+    order_form = Orderform()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+    return render(request, template, context)
+    
