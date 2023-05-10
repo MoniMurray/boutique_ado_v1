2174a61aa923ece8f1399789b4a8b0bc01bf528c
Monica Murray <Monica.rickerby@gmail.com>
2023-05-10 16:19:05 +0000

    Add javasript to Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import Orderform 
+from bag.contents import bag_contents
+
+import stripe 
 
 # Create your views here.
 def checkout(request):
@@ -16,6 +20,10 @@ def checkout(request):
     if not bag:
         messages.error(request, "There is nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
     
     order_form = Orderform()
     template = 'checkout/checkout.html'
