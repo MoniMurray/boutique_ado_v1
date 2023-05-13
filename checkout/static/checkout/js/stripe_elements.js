7403678dd74339914e9242902cfb42dfdcdfd0d4
Monica Murray <Monica.rickerby@gmail.com>
2023-05-13 21:02:02 +0000

    Capture form data in js which cannot be put in paymentIntent and post it to cache_Checkout_data view

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -60,63 +60,79 @@ var style = {
     var form = document.getElementById('payment-form');
 
     // prevent the form's devault action, ie. to POST
-    form.addEventListener('submit', function (ev) {
+    form.addEventListener('submit', function(ev) {
         ev.preventDefault();
         // while the card is being processed by Stripe, disable it to prevent multiple submissions
         card.update({'disabled': true});
         $('#submit-button').attr('disabled', true);
         // form fade out to layout overlay with spinner for payment processing
-        $('payment-form').fadeToggle(100);
+        $('#payment-form').fadeToggle(100);
         $('#loading-overlay').fadeToggle(100);
+
+        var saveInfo = Boolean($('#id-save-info').attr('checked'));
+        var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+        var postData = {
+            'csrfmiddlewaretoken': csrfToken,
+            'client_secret': clientSecret,
+            'save_info': saveInfo,
+        };
+        var url = '/checkout/cache_checkout_data/';
+
+        $.post(url, postData).done(function () {
         // use the stripe confirmcardpayment() method to send the card info securely to Stripe
-        stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: card,
-                billing_details: {
+            stripe.confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: $.trim(form.full_name.value),
+                        phone: $.trim(form.phone_number.value),
+                        email: $.trim(form.email.value),
+                        address: {
+                            line1: $.trim(form.street_address1.value),
+                            line2: $.trim(form.street_address2.value),
+                            city: $.trim(form.town_or_city.value),
+                            country: $.trim(form.country.value),
+                            state: $.trim(form.county.value),
+                        }
+                    }
+                },
+                shipping: {
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
-                    email: $.trim(form.email.value),
                     address: {
-                        line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
-                        city: $.trim(form.town_or_city.value),
-                        country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
+                            line1: $.trim(form.street_address1.value),
+                            line2: $.trim(form.street_address2.value),
+                            city: $.trim(form.town_or_city.value),
+                            postal_code: $.trim(form.post_code.value),
+                            country: $.trim(form.country.value),
+                            state: $.trim(form.county.value),
+                        }
+                },
+            }).then(function(result) {
+                if (result.error) {
+                    var errorDiv = document.getElementById('card-errors');
+                    var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                    $(errorDiv).html(html);
+                    // reverse fade out 
+                    $('#payment-form').fadeToggle(100);
+                    $('#loading-overlay').fadeToggle(100);
+                    // reenable card
+                    card.update({'disabled': false});
+                    $('#submit-button').attr('disabled', false);
+                } else {
+                    if (result.paymentIntent.status === 'succeeded') {
+                        form.submit();
                     }
                 }
-            },
-            shipping: {
-                name: $.trim(form.full_name.value),
-                phone: $.trim(form.phone_number.value),
-                address: {
-                        line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
-                        city: $.trim(form.town_or_city.value),
-                        postal_code: $.trim(form.post_code.value),
-                        country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
-                    }
-            },
-        }).then(function (result) {
-            if (result.error) {
-                var errorDiv = document.getElementById('card-errors');
-                var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-                $(errorDiv).html(html);
-                // reverse fade out 
-                $('payment-form').fadeToggle(100);
-                $('#loading-overlay').fadeToggle(100);
-                // reenable card
-                card.update({'disabled': false});
-                $('#submit-button').attr('disabled', false);
-            } else {
-                if (result.paymentIntent.status === 'succeeded') {
-                    form.submit();
-                }
-            }
-        });
-    });
+            });
+        }).fail(function () {
+            // just reload the page, the error will be in django messages
+            location.reload();
+        })
+});
+   
 
