8132c7454c6e8d3250ea50147935236ae9e40a2e
Monica Murray <Monica.rickerby@gmail.com>
2023-05-13 10:29:52 +0000

    Add code to views.py 'checkout' to enable the order to be saved to the database when a user submits their payment info

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.conf import settings
 from .forms import Orderform 
 from bag.contexts import bag_contents
+from products.models import Product 
+from .models import Order, OrderLineItem
 
 import stripe 
 
@@ -18,36 +20,89 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    # if nothing in the bag
-    if not bag:
-        messages.error(request, "There is nothing in your bag at the moment")
-        return redirect(reverse('products'))
+    # check whether the method is 'post'
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'post_code': request.POST['post_code'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address1'],
+            'county': request.POST['county'],
+        }
+        order_form = Orderform(form_data)
 
-    # set the secret key on Stripe
-    stripe.api_key = stripe_secret_key
+        # if the form is valid, we'll save the order, and then iterate 
+        # through the bag items to create each line item(similar to 
+        # context processor code)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order, 
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database"
+                        "Please call us for more information"
+                    ))
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+            request.session['save_info'] = 'save_info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form.\Please check your information')
+    else:
+        bag = request.session.get('bag', {})
+        # if nothing in the bag
+        if not bag:
+            messages.error(request, "There is nothing in your bag at the moment")
+            return redirect(reverse('products'))
 
-    # create the paymentIntent
-    intent = stripe.PaymentIntent.create(
-        amount = stripe_total,
-        currency = settings.STRIPE_CURRENCY,
-    )
-    
-    order_form = Orderform()
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
 
-    if not stripe_public_key:
-        messages.warning(request, f'Stripe public key is missing. \Did you forget to set it in your environment?')
+        # set the secret key on Stripe
+        stripe.api_key = stripe_secret_key
 
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+        # create the paymentIntent
+        intent = stripe.PaymentIntent.create(
+            amount = stripe_total,
+            currency = settings.STRIPE_CURRENCY,
+        )
+        
+        order_form = Orderform()
 
-    return render(request, template, context)
+        if not stripe_public_key:
+            messages.warning(request, f'Stripe public key is missing. \Did you forget to set it in your environment?')
+
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
+        return render(request, template, context)
     
