21f49f0f754774082e241548fc962c8b0fca04d0
Monica Murray <Monica.rickerby@gmail.com>
2023-05-13 16:38:57 +0000

    Add the WH handler to webhooks.py by passing the event along to it

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -38,4 +38,31 @@ def webhook(request):
         # to catch any other exceptions other than the 2 from Stripe above
         return HttpResponse(content=e, status=400)
     
+    # There are over 100 different webhooks from Stripe
+    # so passing the event along to a WH Handler in a Class is best as we could 
+    #  even import it into other projects or even OpenSource it.
+
+    # set up a webhook handler
+    handler = StripeWH_Handler(request)
+
+    # map webhook events to relevant handler functions
+    # in this dictionary, the keys are the wh names coming from Stripe, and 
+    # the values will be the actual methods inside the handler
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
+
+    # get the webhook type (key) from Stripe
+    event_type = event['type']
+
+    # if there's a handler for it, get it from the event_map or use 
+    # the generic one by default
+    event_handler = event_map.get(event_type, hander.handle_event)
+
+    # call the event handler with the event
+    response = event_handler(event)
+    return response 
+
+
     
