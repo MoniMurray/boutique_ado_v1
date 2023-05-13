5325c2983f5b4bd4365a5a78b60b2ac4db3a1be4
Monica Murray <Monica.rickerby@gmail.com>
2023-05-13 14:36:32 +0000

    Create Webhook_handler into which we can add our code to listen for Stripe Webhooks

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse 
+
+class StripeWH_Handler.py
+    """
+    Handle Stripe Webhooks
+    """
+
+    def __init__(self, request):
+        """ the init class method is a setup method that is called every 
+        time an instance of the class is created"""
+
+        self.request = reques
+    
+    def handle_event(self, event):
+        """ Handle a generic/unknown/unexpected webhook event """
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
