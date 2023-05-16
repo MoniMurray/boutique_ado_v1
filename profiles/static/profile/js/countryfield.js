2778d4ea69fd3fd53b114a6caf8b341df075f79e
Monica Murray <Monica.rickerby@gmail.com>
2023-05-16 21:06:48 +0000

    Update the profile view

--- profiles/static/profile/js/countryfield.js
+++ profiles/static/profile/js/countryfield.js
@@ -4,14 +4,15 @@ let countrySelected = $('#id_default_country').val();
 // the value will be an empty string if the first option from the list
 // is selected, so to determine if that's selected we can use this as a boolean
 if(!countrySelected) {
-    $('#id_default_country').css('color', '#aab74c');
+    $('#id_default_country').css('color', '#aab7c4');
 };
 // we want to capture the change event and every time that happens
-// get the value of it (which choice is made)
+// get the value of it (which choice is made), and then determine the 
+// color depending on the choice made
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
-        $(this).css('color', '#aab74c');
+        $(this).css('color', '#aab7c4');
     } else {
         $(this).css('color', '#000');
     }
