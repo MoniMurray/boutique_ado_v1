db616450fbd7c139624142838d26f13538591ac1
Monica Murray <Monica.rickerby@gmail.com>
2023-05-22 13:39:47 +0000

    Product Admin - Edit a product

--- products/views.py
+++ products/views.py
@@ -97,4 +97,32 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+def edit_product(request, product_id):
+    """
+    Edit a Product in the store
+    """
+    # pre-fill the form wiht the product details
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        # instantiate a new instance of the ProductForm from request.POST,
+        # include request.Files to allow for images to be captured, and
+        # tell it the specific instance to update is the 'product' obtained above.
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully edited a Product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please check the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
     
