4d810e8b5617f2e55a49da68547a97faa071ebec
Monica Murray <Monica.rickerby@gmail.com>
2023-05-23 22:02:06 +0000

    Add to Product admin styling for the custom file input widget

--- products/forms.py
+++ products/forms.py
@@ -4,35 +4,18 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
-    """
-    Give Store owners/superusers the ability to add, update and delete products
 
-    """
     class Meta:
-        """
-        This inner metaclass will define the Model and the fields
-        we want to include
-        """
-
         model = Product
         fields = '__all__'
 
-        # replace the image field on the form with the new one which 
-        # utilises our custom widget
-        image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
-        def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-            # override the init() to make changes to some fields
-
-            super().__init__(*args, **kwargs)
-            categories = Category.objects.all()
-            friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
-            # now that we have the friendly names, update the category 
-            # field on the form to display those to the user to choose 
-            # from instead ofusing the id.
-
-            self.fields['category'].choices = friendly_names
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'border-black rounded-0'
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
--- products/widgets.py
+++ products/widgets.py
@@ -3,13 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomClearableFileInput(ClearableFileInput):
-    """
-    Our custom file inherits the built-in django file
-    """
-
-    # override the clear checkbox label
     clear_checkbox_label = _('Remove')
-    # give the following variables our own custom values
     initial_text = _('Current Image')
     input_text = _('')
     template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
